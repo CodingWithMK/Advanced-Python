822e641f8fe6da22348b820f21e7e2f9fa77502b
Muhammed Musab Kaya <msbkya@unknown822a6ee91e82.home>
2025-10-26 17:10:19 +0300

    New methods for path object manipulation added.

--- Python-Pathlib-Module/03_path-objects.py
+++ Python-Pathlib-Module/03_path-objects.py
@@ -16,3 +16,29 @@ data_dir = Path("data")
 # Combining multiple paths
 file_path = base_path / data_dir / "prices.csv"  
 print(file_path)
+
+
+
+# ============ Creating path objects fro the current working directory ============
+
+cwd = Path.cwd()
+
+print(cwd)
+
+
+
+# ============ Creating path objects from the home working directory ============
+
+home = Path.home()
+
+home / "downloads" / "projects"
+
+
+"""
+NOTE: The Path class itself doesn't perform any file system operations 
+such as path validation, creating directories or files. It is designed 
+for representing and manipulating paths. To actually interact with the 
+file system (checking existence, reading/writing files), we will have 
+to use special methods of Path objects and for some advanced cases, get 
+help from the os module.
+"""
