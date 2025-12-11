bafb8498cf86c87fb0e482a825901d078704d306
Muhammed Musab Kaya <musabk2002@gmail.com>
2025-12-12 02:31:29 +0300

    refactor: password generation function implemented to dataclass.

--- clean-code-fundamentals/pw_entry.py
+++ clean-code-fundamentals/pw_entry.py
@@ -2,19 +2,18 @@ from dataclasses import dataclass
 import string
 import secrets
 
-alphabet = string.ascii_letters + string.digits + string.punctuation
+@dataclass
+class PasswordEntry:
+    website: str
+    email: str
+    password: str
+    username: str = ""
 
-def generate_password(length: int = 12) -> str:
-    generated = [secrets.choice(alphabet) for _ in range(length)]
-    generated_pw = "".join(generated)
+    def generate_password(length: int = 12) -> str:
+        alphabet = string.ascii_letters + string.digits + string.punctuation
+        generated = [secrets.choice(alphabet) for _ in range(length)]
+        generated_pw = "".join(generated)
+        
+        return generated_pw
     
-    return generated_pw
-
-print(generate_password())
-
-# @dataclass
-# class PasswordEntry:
-#     website: str
-#     email: str
-#     password: str
-#     username: str = ""
+    # TODO: Create an entry using the generate_password() function. -> Tip: create_entry(website: str, email: str, username: str = "") -> PasswordEntry
