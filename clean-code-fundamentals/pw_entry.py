a39315ca90d9c45511e5593a96f974d5d4af25bc
Muhammed Musab Kaya <musabk2002@gmail.com>
2025-12-21 04:26:46 +0300

    feat: Functionality for creating new entry with random password.

--- clean-code-fundamentals/pw_entry.py
+++ clean-code-fundamentals/pw_entry.py
@@ -2,18 +2,27 @@ from dataclasses import dataclass
 import string
 import secrets
 
+def generate_password(length: int = 12) -> str:
+    alphabet = string.ascii_letters + string.digits + string.punctuation
+    generated = [secrets.choice(alphabet) for _ in range(length)]
+    generated_pw = "".join(generated)
+    
+    return generated_pw
+
 @dataclass
 class PasswordEntry:
     website: str
     email: str
     password: str
     username: str = ""
-
-    def generate_password(length: int = 12) -> str:
-        alphabet = string.ascii_letters + string.digits + string.punctuation
-        generated = [secrets.choice(alphabet) for _ in range(length)]
-        generated_pw = "".join(generated)
-        
-        return generated_pw
     
-    # TODO: Create an entry using the generate_password() function. -> Tip: create_entry(website: str, email: str, username: str = "") -> PasswordEntry
+    # Create an entry using the generate_password() function.
+    def create_entry(website: str, email: str, username: str=""):
+        website = website
+        email = email
+        generated_password = generate_password()
+        
+        return website, email, generated_password, username
+    
+if __name__ == "__main__":
+    print(PasswordEntry.create_entry(website="Google", email="jack123@gmail.com"))
