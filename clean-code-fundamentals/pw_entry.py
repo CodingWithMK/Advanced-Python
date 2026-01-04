b3573d0d9fc1d3a23532a07dbb07d5dc3d9c696e
Musab Kaya <musabkaya@MacBookvonMusab.home>
2026-01-05 02:20:38 +0300

    feat: Path handling for database.

--- clean-code-fundamentals/pw_entry.py
+++ clean-code-fundamentals/pw_entry.py
@@ -3,8 +3,6 @@ from pathlib import Path
 import string
 import secrets
 
-home = Path.home()
-
 def generate_password(length: int = 12) -> str:
     alphabet = string.ascii_letters + string.digits + string.punctuation
     generated = [secrets.choice(alphabet) for _ in range(length)]
@@ -12,8 +10,12 @@ def generate_password(length: int = 12) -> str:
     
     return generated_pw
 
-def get_database(path: Path) -> None:
-    pass
+def get_db_path() -> Path:
+    home = Path.home()
+    folder_path = home / '.my_password_manager'
+    folder_path.mkdir(parents=True, exist_ok=True)
+    return folder_path / "passwords.db"
+
 
 @dataclass
 class PasswordEntry:
@@ -30,4 +32,5 @@ class PasswordEntry:
         return cls(website, email, generated_password, username)
     
 if __name__ == "__main__":
-    print(PasswordEntry.create_entry(website="Google", email="jack123@gmail.com"))
+    # print(PasswordEntry.create_entry(website="Google", email="jack123@gmail.com"))
+    print(get_db_path())
