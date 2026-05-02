9c6b33623003ebb9128c7f0491248d2cf0110b27
Muhammed Musab Kaya <musabk2002@gmail.com>
2026-05-03 02:40:26 +0300

    feat: Add method for grouping files by their sizes.

--- Automation/duplicate_file_finder.py
+++ Automation/duplicate_file_finder.py
@@ -2,6 +2,7 @@ from pathlib import Path
 from typing import Optional, Literal
 from dataclasses import dataclass
 import hashlib
+import send2trash
 import os
 
 @dataclass
@@ -9,14 +10,18 @@ class DuplicateFinder:
     dir_path: Path
     chunk_size: int = 65536
     file_size: float
-    files_by_size: dict[int, list[Path]]
+    files_by_size: dict[str, dict[int, list[Path]]] = {
+        "<8MB": {},
+        "<16MB": {},
+        ">16MB": {}
+    }
     file_status: Literal["scanning", "hashing", "completed", "error"]
     duplicates: dict
     potential_duplicates: tuple[Path, ...]
     hash_algorithm: Literal["md5", "sha1", "sha256"]
-    min_size: int
+    min_size: int = 2048
     scan_results: str
-    ingnored_extensions: set[str] = {}
+    ignored_extensions: set[str] = {}
     fast_scan: bool = False
     
     def __post_init__(self):
@@ -33,8 +38,31 @@ class DuplicateFinder:
                     with open(entry, "rb", buffering=self.chunk_size) as file:
                         hashlib.sha256(file)
 
-    def scan(self):
-        pass
+    def scan(self) -> list[Path]:
+        self.files = [file for file in self.dir_path.iterdir() if file.is_file() and file.stat().st_size >= 2048 and file.suffix not in self.ignored_extensions]
+
+        return self.files
+    
+    def group_by_size(self) -> dict[str, dict[int, list[Path]]]:
+        """
+        Adds every file by their size in the exact size of bytes,
+        and categorizes them by the matching size label.
+        """
+        for file in self.scan:
+            file_size_bytes = file.stat().st_size
+            file_size_mb = file_size_bytes / (1024 * 1024)
+
+            # Determine category
+            category = "<8MB" if file_size_mb < 8 else "<16MB" if file_size_mb < 16 else ">16MB"
+
+            # Call into inner dictionary branch
+            inner_dict = self.files_by_size[category]
+
+            inner_dict.setdefault(category, []).append(file)
+
+            return self.files_by_size
+
+    
 
         
         
