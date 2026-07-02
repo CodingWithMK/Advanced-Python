f7a77907e4a9c04b90dedc272a2c98e0595efc7b
Muhammed Musab Kaya <musabk2002@gmail.com>
2026-07-02 19:42:10 +0300

    feat: implement multi-stage size and partial hash grouping filtering

--- Automation/duplicate_file_finder.py
+++ Automation/duplicate_file_finder.py
@@ -11,13 +11,13 @@ class DuplicateFinder:
     chunk_size: int = 65536
     file_size: float
     files_by_size: dict[str, dict[int, list[Path]]] = {
-        "<8MB": {},
-        "<16MB": {},
+        "<=8MB": {},
+        "<=16MB": {},
         ">16MB": {}
     }
     file_status: Literal["scanning", "hashing", "completed", "error"]
     duplicates: dict
-    potential_duplicates: tuple[Path, ...]
+    potential_duplicates: list[tuple[Path, ...]]
     hash_algorithm: Literal["md5", "sha1", "sha256"]
     min_size: int = 2048
     scan_results: str
@@ -53,14 +53,56 @@ class DuplicateFinder:
             file_size_mb = file_size_bytes / (1024 * 1024)
 
             # Determine category
-            category = "<8MB" if file_size_mb < 8 else "<16MB" if file_size_mb < 16 else ">16MB"
+            category = "<=8MB" if file_size_mb <= 8 else "<=16MB" if file_size_mb <= 16 else ">16MB"
 
             # Call into inner dictionary branch
             inner_dict = self.files_by_size[category]
 
-            inner_dict.setdefault(category, []).append(file)
+            inner_dict.setdefault(file_size_bytes, []).append(file)
 
-            return self.files_by_size
+        return self.files_by_size
+    
+    def group_by_partial_hash(self) -> list[tuple[Path, ...]]:
+        """
+        Gets grouped files of file_by_size and scans first 64KB of every file.
+        If first partial hash matches it returns them as a group for full hash
+        scan.
+        """
+        partial_hash_groups = {}
+
+        # iterating through categories
+        for category, inner_dict in self.files_by_size.items():
+            # iterating through exact bytesizes and the corresponding file list
+            for byte_size, file_list in inner_dict.items():
+                if len(file_list) < 2:
+                    continue
+
+                for file_path in file_list:
+                    try:
+                        hasher = hashlib.new(self.hash_algorithm)
+
+                        with open(file_path, "rb") as file:
+                            chunk = file.read(self.chunk_size)
+                            hasher.update(chunk)
+
+                        partial_hash = hasher.hexdigest()
+
+                        # Grouping the file paths by their partial hashes
+                        partial_hash_groups.setdefault(partial_hash, []).append(file_path)
+
+                    except (PermissionError, OSError):
+                        continue                        # skip compromised/blocked files
+                
+        # Filtering the groups of potential duplicates
+        temp_list = []
+        for p_hash, paths in partial_hash_groups.items():
+                if len(paths) > 1:                          # if more than one file in partial hash then add to temp_list
+                    temp_list.append(tuple(paths))
+
+        self.potential_duplicates = temp_list
+
+        return self.potential_duplicates
+
 
     
 
